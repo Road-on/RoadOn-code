eb9cccf586123b7e6053f2f1a5952ac5db2b0711
stFabio <sntfabio.22@gmail.com>
2021-09-22 17:43:01 -0300

    Correção de parâmetros no controller de empresa

--- api/src/controllers/empresa.controller.js
+++ api/src/controllers/empresa.controller.js
@@ -18,34 +18,38 @@ exports.createEmpresa = async (req, res) => {
 };
 
 // ==> Método responsável por listar todos as Empresas':
+
 exports.listAllEmpresas = async (req, res) => {
   const response = await db.query('SELECT * FROM empresa ORDER BY nome_empresa ASC');
   res.status(200).send(response.rows);
 };
 
 // ==> Método responsável por selecionar Empresa pelo 'Id':
+
 exports.findEmpresaById = async (req, res) => {
-  const empresaId = parseInt(req.params.id);
-  const response = await db.query('SELECT * FROM empresa WHERE empresaid = $1', [productId]);
+  const id_empresa = parseInt(req.params.id);
+  const response = await db.query('SELECT * FROM empresa WHERE id_empresa = $1', [id_empresa]);
   res.status(200).send(response.rows);
 }
 
-// ==> Método responsável por atualizar um 'Product' pelo 'Id':
+// ==> Método responsável por atualizar uma Empresa pelo 'Id':
+
 exports.updateEmpresaById = async (req, res) => {
-  const empresaId = parseInt(req.params.id);
+  const id_empresa = parseInt(req.params.id);
   const { nome_empresa, cnpj_empresa, telefone_empresa, email_empresa, senha_empresa} = req.body;
   const response = await db.query(
-    "UPDATE empresa SET nome_empresa = $1, cnpj_empresa = $2, telefone_empresa = $3, email_empresa = $4, senha_empresa = $5 WHERE empresaId = $6",
-    [product_name, quantity, price, productId]
+    "UPDATE empresa SET nome_empresa = $1, cnpj_empresa = $2, telefone_empresa = $3, email_empresa = $4, senha_empresa = $5 WHERE id_empresa = $6",
+    [nome_empresa, cnpj_empresa, telefone_empresa, email_empresa, senha_empresa, id_empresa]
   );
-  res.status(200).send({ message: "Empresa Atualizada com sucesso!" });
+  res.status(200).send({ message: "Empresa atualizada com sucesso!" });
 };
 
 // ==> Método responsável por excluir um 'Product' pelo 'Id':
+
 exports.deleteEmpresaById = async (req, res) => {
-  const empresaId = parseInt(req.params.id);
-  await db.query('DELETE FROM empresa WHERE empresaid = $1', [
-    productId
+  const id_empresa = parseInt(req.params.id);
+  await db.query('DELETE FROM empresa WHERE id_empresa = $1', [
+    id_empresa
   ]);
-  res.status(200).send({ message: 'Empresa deletada com sucesso!', productId });
+  res.status(200).send({ message: 'Empresa deletada com sucesso!', id_empresa });
 };
--- api/src/routes/empresa.routes.js
+++ api/src/routes/empresa.routes.js
@@ -3,15 +3,15 @@
     * Arquivo: src/routes/empresa.routes.js
     * Descrição: arquivo responsável pelas rotas da api relacionado a classe 'Empresa'.
     * Data: 10/09/2021
-    * Author: Fabio Santos
+    * Author: Fabio Santos / Gustavo Morais
  */
  const router = require('express-promise-router')();
+
  const empresaController = require('../controllers/empresa.controller');
  // ==> Definindo as rotas do CRUD - 'Empresa':
+
  // ==> Rota responsável por criar uma nova Empresa: (POST): localhost:3000/api/empresas
  router.post('/empresas', empresaController.createEmpresa);
- module.exports = router;
-
  // ==> Rota responsável por listar todas as Empresas: (GET): localhost:3000/api/empresas
 router.get('/empresas', empresaController.listAllEmpresas);
 
@@ -23,3 +23,5 @@ router.put('/empresas/:id', empresaController.updateEmpresaById);
 
 // ==> Rota responsável por excluir Empresa pelo 'Id': (DELETE): localhost:3000/api/empresas/:id
 router.delete('/empresas/:id', empresaController.deleteEmpresaById);
+
+module.exports = router;
