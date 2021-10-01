d9d0e208492b98b7bfcedac2e7886070f87ab798
stFabio <sntfabio.22@gmail.com>
2021-10-01 00:02:06 -0300

    Correção tabelas do banco de dados

--- database/database.sql
+++ database/database.sql
@@ -9,21 +9,20 @@ CREATE TABLE IF NOT EXISTS Empresa(
 
 CREATE TABLE IF NOT EXISTS Destino(
     id_destino SERIAL NOT NULL PRIMARY KEY,
-    nome_destino varchar(80) NOT NULL,
+    nome_destino VARCHAR(80) NOT NULL,
 	id_empresa SERIAL NOT NULL,
+    valor_excursao DECIMAL NOT NULL,
+    minimo_passageiro_excursao INTEGER NOT NULL, 
+    maximo_passageiro_excursao INTEGER NOT NULL, 
 	
 	CONSTRAINT fk_id_empresa FOREIGN KEY (id_empresa) REFERENCES Empresa(id_empresa)
 );
 
-CREATE TABLE IF NOT EXISTS Excursao (
+CREATE TABLE IF NOT EXISTS Agenda_Excursao (
     id_excursao SERIAL NOT NULL PRIMARY KEY,
     id_destino SERIAL NOT NULL,
     data_saida_excursao DATE NOT NULL,
     data_volta_excursao DATE NOT NULL,
-    valor_excursao DECIMAL NOT NULL,
-    minimo_passageiro_excursao SERIAL NOT NULL, 
-    maximo_passageiro_excursao INTEGER NOT NULL, 
-    aluguel_onibus_excursao DECIMAL NOT NULL,
 
     CONSTRAINT fk_id_destino FOREIGN KEY (id_destino) REFERENCES Destino(id_destino)
 );
@@ -40,6 +39,6 @@ CREATE TABLE IF NOT EXISTS Pessoa_Excursao(
     id_excursao SERIAL NOT NULL,
 	
     CONSTRAINT fk_id_pessoa FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id_pessoa),
-    CONSTRAINT fk_id_excursao FOREIGN KEY (id_excursao) REFERENCES Excursao(id_excursao),
+    CONSTRAINT fk_id_excursao FOREIGN KEY (id_excursao) REFERENCES Agenda_Excursao(id_excursao),
 	CONSTRAINT pk_id_excursao_pessoa PRIMARY KEY (id_excursao, id_pessoa)
 );
