b6e3f26f1f85f070cb05c6bba7eaf9c529965432
gustavomoraiss <gustavo.morais1098@gmail.com>
2021-10-02 01:27:12 -0300

    Correção de comentário

--- src/routes/agendaExcursao.routes.js
+++ src/routes/agendaExcursao.routes.js
@@ -10,19 +10,19 @@
  const agendaExcursaoController = require('../controllers/agendaExcursao.controller');
  // ==> Definindo as rotas do CRUD - 'Excursões':
 
- // ==> Rota responsável por criar uma nova Excursão: (POST): localhost:3000/api/agendamentos
+ // ==> Rota responsável por criar uma nova Agendamento: (POST): localhost:3000/api/agendamentos
  router.post('/agendamentos', agendaExcursaoController.createExcursao);
  
- // ==> Rota responsável por listar todas as Excursão: (GET): localhost:3000/api/agendamentos
+ // ==> Rota responsável por listar todas as Agendamento: (GET): localhost:3000/api/agendamentos
 router.get('/agendamentos', agendaExcursaoController.listAllExcursoes);
 
-// ==> Rota responsável por selecionar Excursão pelo 'Id': (GET): localhost:3000/api/agendamentos/:id
+// ==> Rota responsável por selecionar Agendamento pelo 'Id': (GET): localhost:3000/api/agendamentos/:id
 router.get('/agendamentos/:id', agendaExcursaoController.findExcursaoById);
 
-// ==> Rota responsável por atualizar Excursão pelo 'Id': (PUT): localhost: 3000/api/agendamentos/:id
+// ==> Rota responsável por atualizar Agendamento pelo 'Id': (PUT): localhost: 3000/api/agendamentos/:id
 router.put('/agendamentos/:id', agendaExcursaoController.updateExcursaoById);
 
-// ==> Rota responsável por excluir Excursão pelo 'Id': (DELETE): localhost:3000/api/agendamentos/:id
+// ==> Rota responsável por excluir Agendamento pelo 'Id': (DELETE): localhost:3000/api/agendamentos/:id
 router.delete('/agendamentos/:id', agendaExcursaoController.deleteExcursaoById);
 
 module.exports = router;
