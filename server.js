9bad4c6bde2d17609adb328224e3302c2b43084a
HenriqueGanso <henrique.devel@gmail.com>
2021-10-03 22:15:41 -0300

    cRIANDO VIEW

--- server.js
+++ server.js
@@ -4,7 +4,17 @@
      * Data: 10/09/2021
      * Author: Fabio Santos
  */
- const app = require('./src/app');
- app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+
+
+const express = require('express');
+const app = express();
+app.set('view-engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index.ejs', {nome: 'João'});
+})
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs', {titulo: 'João'});
+})
+
+app.listen(3000);
