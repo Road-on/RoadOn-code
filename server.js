2d77bd28468f41fee16335859e0a702b9a00364d
stFabio <sntfabio.22@gmail.com>
2021-10-05 10:34:02 -0300

    Correção de portas do servidor

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 
 const express = require('express');
 const app = express();
+ 
 app.set('view-engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index.ejs', {nome: 'João'});
@@ -17,4 +18,7 @@ app.get('/login', (req, res) => {
   res.render('login.ejs', {titulo: 'João'});
 })
 app.use(express.static(__dirname + '/front-end'));
-app.listen(3000);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
